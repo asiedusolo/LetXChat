a8b5dd4d396b5215480f4f25e1aa206120ff7284
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-19 10:22:08 +0000

    adding basic pages setup and react-router links

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+import LandingPage from './pages/landingPage/landingPage';
+import Register from './pages/register/register';
+import Login from './pages/login/login';
+import ChatPage from './pages/chatPage/chatPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>LetXChat</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/chatroom">
+          <ChatPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/pages/chatPage/chatPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const ChatPage = () => {
+    return (
+        <div>
+            <h1>Enter a chat room and begin chatting</h1>
+        </div>
+    )
+ }
+
+
+ export default ChatPage
