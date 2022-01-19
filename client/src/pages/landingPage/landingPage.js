9d6103fca84d520b71fa0857b2738e353d458120
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-19 12:49:57 +0000

    adding sign up and login buttons to landing page

--- client/src/pages/landingPage/landingPage.js
+++ client/src/pages/landingPage/landingPage.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
-    return (
-        <div>
-            <h1>Landing Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <button><Link to="/register">Sign up</Link></button>
+      <button><Link to="/login">Login</Link></button>
+    </div>
+  );
+};
 
-
-export default LandingPage
+export default LandingPage;
