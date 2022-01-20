cfce00adb0d96dab97c3fb7f8f80ad95cfea9a1e
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-20 15:37:56 +0000

    adding authentication action creators

--- client/src/contexts/auth/authcontext.js
+++ client/src/contexts/auth/authcontext.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useReducer, createContext } from 'react';
-import AuthReducer from './authReducer';
+import React, { useEffect, useReducer, createContext } from "react";
+import AuthReducer from "./authReducer";
 
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem("user")) || null,
-    isFetchingUser: false,
-    error: false
-}
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isFetchingUser: false,
+  error: false,
+};
 
-export const AuthContext = createContext(INITIAL_STATE)
-
+export const AuthContext = createContext(INITIAL_STATE);
 
 const AuthcontextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE)
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-    useEffect(() => {
-        localStorage.setItem("user", JSON.stringify(state.user))
-     },
-    [state.user])
-    return (
-        <AuthContext.Provider>
-            value={{
-                user: state.user,
-                isFetchingUser: state.isFetchingUser,
-                error: state.error,
-                dispatch
-            }}
-        </AuthContext.Provider>
-    )
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetchingUser: state.isFetchingUser,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthcontextProvider;
