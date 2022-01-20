d8fd176bc1bce28f27b2bf0ae6c1fc1fe38c842b
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-20 16:11:18 +0000

    adding login api call functionality

---
+++ client/src/pages/login/loginAPICalls.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+
+const LoginCall = async (userCredentials, dispatch) => {
+    dispatch({ type: "LOGIN_START" })
+    try {
+        const response = await axios.post("http://localhost:5000/api/auth/login", userCredentials)
+        dispatch({ type: "LOGIN_SUCCESS", payload: response.data})
+    } catch (error) {
+        dispatch({type: "LOGIN_FAILURE", payload: error})
+    }
+}
+
+
+export default LoginCall
