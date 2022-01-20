8c63c4caa670095abb02d52a30365f756ae850cf
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-20 12:57:36 +0000

    adding getting user and updating user route functionality

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ const app = express()
 const port = 5000
 const connectDB = require('./db/connect')
 const authRouter = require('./routes/auth')
+const userRouter = require('./routes/users')
 require('dotenv').config()
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
 app.use(cors())
 
 
@@ -17,6 +21,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 const start = async() => {
     try {
---
+++ server/routes/users.js
@@ -0,0 +1,48 @@
+const User = require("../models/User");
+const express = require("express");
+const router = express.Router();
+const bycrypt = require("bcrypt");
+const saltRounds = 10;
+
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
+
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  if (req.body.password) {
+    try {
+      const salt = await bcrypt.genSalt(saltRounds);
+      req.body.password = await bcrypt.compare(req.body.password, salt);
+    } catch (error) {
+      return res.status(500).json(err);
+    }
+  }
+  try {
+    const user = await User.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+    if (!user) {
+      return res
+        .status(404)
+        .json({ msg: `No user with id ${req.params.id} found` });
+    }
+    return res.status(200).json(user._doc);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
