d5f8a9f45eb6a7fb9690ecb7d56841cbfb0bb8fe
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-21 15:59:35 +0000

    router user to profile page

--- client/src/components/topbar/topbar.js
+++ client/src/components/topbar/topbar.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./topbar.css"
-
+import { Link } from 'react-router-dom'
+import { useContext } from "react";
+import {AuthContext} from "../../contexts/auth/authcontext.js"
 
 const Topbar = () => {
+    const { user } = useContext(AuthContext)
   return (
     <div className="topbar">
-      <div className="topbarWrapper">
-        <div>
-          <div >
-            <img
-              src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreviews.123rf.com%2Fimages%2Fkritchanut%2Fkritchanut1406%2Fkritchanut140600093%2F29213195-male-silhouette-avatar-profile-picture.jpg&imgrefurl=https%3A%2F%2Fwww.123rf.com%2Fphoto_29213195_male-silhouette-avatar-profile-picture.html&tbnid=BJGkF8iyujvTDM&vet=12ahUKEwjepOn8icP1AhU4XvEDHei7ApwQMygMegUIARD-AQ..i&docid=mKadYBOkjP6bnM&w=1300&h=1300&q=profile%20avatar&ved=2ahUKEwjepOn8icP1AhU4XvEDHei7ApwQMygMegUIARD-AQ"
+        <div className="profileSection">
+          <div className="imgContainer">
+                  <Link to={`/profile/${user.username}`}>
+                  <img
+              src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
               alt="profilePicture"
             />
+                  </Link>
           </div>
-          <h4>username</h4>
         </div>
-      </div>
     </div>
   );
 };
