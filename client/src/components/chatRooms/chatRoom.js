9a5e0014b0ad730e1987e607ea8f81e866bff90b
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-24 12:38:36 +0000

    adding props to chatRoom component

--- client/src/components/chatRooms/chatRoom.js
+++ client/src/components/chatRooms/chatRoom.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './chatRoom.css'
 
-const ChatRoom = () => {
+const ChatRoom = ({chatRoomName, avatarUrl}) => {
     return (
         <div className="chatRoom">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3KNxefYnky1ecrnryOJUJ6Gjun5IN7rHZoA&usqp=CAU" alt="" className="chatRoomImg" />
-            <span className="chatRoomName">Full Stack</span>
+            <span className="chatRoomName">{chatRoomName}</span>
         </div>
     )
 }
