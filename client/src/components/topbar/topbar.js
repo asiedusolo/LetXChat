0e2eb490a66124528c63c26a642f7f489106aba4
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-28 18:16:59 +0000

    styling the topbar

--- client/src/components/topbar/topbar.js
+++ client/src/components/topbar/topbar.js
@@ -1,23 +1,27 @@
 import React from "react";
-import "./topbar.css"
-import { Link } from 'react-router-dom'
+import "./topbar.css";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
-import {AuthContext} from "../../contexts/auth/authcontext.js"
+import { AuthContext } from "../../contexts/auth/authcontext.js";
 
 const Topbar = () => {
-    const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   return (
     <div className="topbar">
-        <div className="profileSection">
-          <div className="imgContainer">
-                  <Link to={`/profile/${user.username}`}>
-                  <img
+      <div className="profileSection">
+        <div className="chatLogo">
+          <h4>LetXChat</h4>
+        </div>
+        <div className="userContainer">
+          <p class="userName">{user.name}</p>
+          <Link to={`/profile/${user.username}`}>
+            <img
               src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
               alt="profilePicture"
             />
-                  </Link>
-          </div>
+          </Link>
         </div>
+      </div>
     </div>
   );
 };
