10a0fbf2d60be0c4475bd1aaee6af0a25c20edfb
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-29 12:47:11 +0000

    installing multer and adding file upload path

--- server/index.js
+++ server/index.js
@@ -1,42 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const connectDB = require('./db/connect')
-const authRouter = require('./routes/auth')
-const userRouter = require('./routes/users')
-const messageRouter = require('./routes/messages')
-const chatRoomRouter = require('./routes/chatRooms')
-require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = 5000;
+const connectDB = require("./db/connect");
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/users");
+const messageRouter = require("./routes/messages");
+const chatRoomRouter = require("./routes/chatRooms");
+require("dotenv").config();
+const cors = require("cors");
 
-app.use(express.json())
-app.use(express.urlencoded({
+const multer = require("multer");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
     extended: false
-}))
-app.use(cors())
+  })
+);
+app.use(cors());
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  }
+});
 
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/messages", messageRouter);
+app.use("/api/chatRooms", chatRoomRouter);
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("DB Connection established");
+    app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
-app.use('/api/messages', messageRouter)
-app.use('/api/chatRooms', chatRoomRouter)
-
-
-const start = async() => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        console.log('DB Connection established')
-        app.listen(port, () => console.log(`Server is listening on port ${port}!`))
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-start()
+start();
