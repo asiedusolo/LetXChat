be1a03e8d3b8f6d09315fe002354d9c804fd5f11
asiedusolo <solomon.asiedu@amalitech.org>
2022-01-29 11:37:18 +0000

    styling the profile page

--- client/src/pages/profile/profile.js
+++ client/src/pages/profile/profile.js
@@ -21,34 +21,56 @@ const Profile = () => {
   return (
     <div className="profilePage">
       <div className="userProfileData">
-        <h3>{user.name}</h3>
-        <img
-          alt="profilePic"
-          src={
-            user.picture_avatar
-              ? user.picture_avatar
-              : `https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png`
-          }
-        />
-        <h3>{user.username}</h3>
-        <h3>{user.staff_email}</h3>
-        <input type="file" name="picturePicture" />
+        <div className="userProfileWrapper">
+          <h3 className="userName">{user.name}</h3>
+          <img
+            alt="profilePic"
+            src={
+              user.picture_avatar
+                ? user.picture_avatar
+                : `https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png`
+            }
+            className="userImage"
+          />
+          <h3>{user.username}</h3>
+          <h3>{user.staff_email}</h3>
+          <input type="file" name="picturePicture" />
+          <button type="submit">Upload profile</button>
+        </div>
       </div>
       <div className="userProfileEdit">
-        <form>
-          <input type="text" name="name" placeholder="Full Name" />
+        <form className="userProfileForm">
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            className="profileTextField"
+          />
           <br />
-          <input type="text" name="username" placeholder="Username" />
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            className="profileTextField"
+          />
           <br />
-          <input type="password" name="password" placeholder="Password" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="profilePasswordField"
+          />
           <br />
           <input
             type="password"
             name="confirmPassword"
             placeholder="Confirm Password"
+            className="profilePasswordField"
           />
           <br />
-          <button type="submit">Edit Profile</button>
+          <button type="submit" className="profileEditButton">
+            Edit Profile
+          </button>
         </form>
       </div>
     </div>
