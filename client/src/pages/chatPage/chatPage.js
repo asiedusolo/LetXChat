12564ac2ad7b48f15e1c8a5d8259d80f4adbd11e
asiedusolo <88283317+asiedusolo@users.noreply.github.com>
2025-06-09 11:31:40 +0200

    using correct env name

--- client/src/pages/chatPage/chatPage.js
+++ client/src/pages/chatPage/chatPage.js
@@ -284,7 +284,7 @@ const ChatPage = () => {
   });
   
   useEffect(() => {
-    socket.current = io(process.env.REACT_APP_SOCKET_URL || "http://localhost:8900", {
+    socket.current = io(process.env.REACT_APP_SOCKET_BASE_URL || "http://localhost:8900", {
       path: '/socket.io',
       transports: ['websocket']
     });
