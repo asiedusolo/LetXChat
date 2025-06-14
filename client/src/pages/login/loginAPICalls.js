a264288e71c2f34c09e44b76213bbe1c36632ffa
asiedusolo <88283317+asiedusolo@users.noreply.github.com>
2025-06-14 09:20:42 +0200

    adding logs

--- client/src/pages/login/loginAPICalls.js
+++ client/src/pages/login/loginAPICalls.js
@@ -4,6 +4,7 @@ const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 const LoginCall = async (userCredentials, dispatch) => {
     dispatch({ type: "LOGIN_START" })
     try {
+        console.log({REACT_APP_API_BASE_URL: REACT_APP_API_BASE_URL})
         const response = await axios.post(`${REACT_APP_API_BASE_URL}/api/auth/login`, userCredentials)
         dispatch({ type: "LOGIN_SUCCESS", payload: response.data })
         
